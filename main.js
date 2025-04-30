fc0ec283bf4f8b60e05911e9f6959762e36467cb
satyam123p <satyamkumar13569@gmail.com>
2025-04-30 11:16:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,53 @@
-import * as uiUtils from "./modules/uiUtils.js";
+import { initializeUi, DOM,exitRoom, addOutgoingMessageToUi} from "./modules/uiUtils.js";
 import * as ws from "./modules/ws.js";
+import * as ajax from "./modules/ajax.js";
 import * as state from "./modules/state.js";
 import * as webRTCHandler from "./modules/webRTCHandler.js";
+
 // Generate unique user code for every user that visits the page
 const userId = Math.round(Math.random() * 1000000);
-// set userId
-state.setUserId(userId);
+
+// initialize the DOM
+initializeUi(userId);
+
 // establish a ws connection
-const wsClientConnection = new WebSocket(`ws://localhost:8080/?userId=${userId}`);
+const wsClientConnection = new WebSocket(`ws://107.109.204.190:8080/?userId=${userId}`);
+
 // pass all of our websocket logic to another module
 ws.registerSocketEvents(wsClientConnection);
-// destroying a room (before peer2 has entered/joined the room)
-uiUtils.DOM.destroyRoomButton.addEventListener("click", () => {
+
+// create room
+DOM.createRoomButton.addEventListener("click", () => {
+   const roomName = DOM.inputRoomNameElement.value;
+   DOM.inputRoomNameElement.value="";
+   if(!roomName) {
+    return alert("Your room needs a name");
+   };
+   ajax.createRoom(roomName, userId);
+});
+DOM.destroyRoomButton.addEventListener("click", () => {
    const roomName = state.getState().roomName;
-   fetch('http://localhost:8080/destroy-room', {
-      method: 'POST',
-      headers: {
-         'Content-Type': 'application/json'
-      }, 
-      body: JSON.stringify({roomName})
-   })
-   .then( response => response.json() )
-   .then(resObj => {   
-      if(resObj.data.type === state.type.ROOM_DESTROY.RESPONSE_SUCCESS) {
-         uiUtils.exitRoom();
-      }
-      if(resObj.data.type === state.type.ROOM_DESTROY.RESPONSE_FAILURE) {
-         console.log("Destroy room failure",resObj.data.message);
-      } 
-   })
-   .catch(err => {
-      console.log("an error ocurred trying to destroy a room: ", err);
-   })
+   ajax.destroyRoom(roomName);
 })
-// joining a room (peer2)
-uiUtils.DOM.joinRoomButton.addEventListener("click", () => {
-   const roomName = uiUtils.DOM.inputRoomNameElement.value; 
+DOM.joinRoomButton.addEventListener("click", () => {
+   const roomName = DOM.inputRoomNameElement.value; 
+   DOM.inputRoomNameElement.value="";
    if(!roomName) {
       return alert("You have to join a room with a valid name");
    }
    ws.joinRoom(roomName, userId, wsClientConnection);
 });
-// exit a room 
-uiUtils.DOM.exitButton.addEventListener("click", () => {
+DOM.exitButton.addEventListener("click", () => {
    const roomName = state.getState().roomName;
-   uiUtils.exitRoom();
+   exitRoom();
    ws.exitRoom(roomName, userId);
-   alert(`You have left room ${roomName}`);
-   // close the peer connection and the data channel (if they exist)
    webRTCHandler.closePeerConnection();
+});
+DOM.sendMessageButton.addEventListener("click", () => {
+   const message = DOM.messageInputField.value.trim();
+   DOM.messageInputField.value="";
+   if(message){
+      addOutgoingMessageToUi(message);
+      webRTCHandler.sendMessageUsingDataChannel(message);
+   };
 });
