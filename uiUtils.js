66055a9fcf9a9a5d3970d9caf1268343ffac6d86
satyam123p <satyamkumar13569@gmail.com>
2025-04-30 11:17:09 +0530

    Update uiUtils.js

--- uiUtils.js
+++ uiUtils.js
@@ -1,20 +1,43 @@
 import * as state from "./state.js";
 const inputRoomNameElement = document.getElementById('input_room_channel_name');
 const joinRoomButton = document.getElementById('join_button');
+const createRoomButton = document.getElementById('create_room_button');
+const messageInputField = document.getElementById('message_input_field');
+const sendMessageButton = document.getElementById('send_message_button');
+const destroyRoomButton = document.getElementById('destroy_button');
 const exitButton = document.getElementById('exit_button');
-const destroyRoomButton = document.getElementById('destroyRoomButton');
-export const DOM={
+const messageContainer = document.getElementById("message-container");
+export const DOM = {
+    createRoomButton,
     inputRoomNameElement,
     destroyRoomButton,
     joinRoomButton,
     exitButton,
+    sendMessageButton,
+    messageInputField,
+}
+export function initializeUi(userId) {
+    state.setUserId(userId);
 }
 export function exitRoom() {
     inputRoomNameElement.value = '';
     state.resetState();
 }
+export function updateUiForRemainingUser() {
+    alert("a user has left your room");
+    state.setOtherUserId(null);
+}
+export function addOutgoingMessageToUi(message) {
+    const userTag = "YOU";
+    const formattedMessage = `${userTag}: ${message}`;
+    const p = document.createElement("p");
+    p.textContent = formattedMessage;
+    messageContainer.appendChild(p);
+}
 export function addIncomingMessageToUi(msg) {
     const otherUserId = state.getState().otherUserId;
     const formattedMessage = `${otherUserId}: ${msg}`;
-    console.log(formattedMessage);
+    const p = document.createElement("p");
+    p.textContent = formattedMessage;
+    messageContainer.appendChild(p);
 }
